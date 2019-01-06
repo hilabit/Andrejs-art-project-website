d8b89c055f39075f88bf379baa4b30dab43564b7
Hila Bitterman <hilabitterman@Hilas-MBP.fritz.box>
2019-01-06 21:10:11 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const bodyParser= require('body-parser');
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+
+const portNumber = 2777;
+
+app.use('/public', express.static(__dirname + '/public'));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.render('home', {
+        layout: 'layout',
+    });
+});
+
+app.listen(portNumber, ()=> console.log(`listening on ${portNumber}`));
