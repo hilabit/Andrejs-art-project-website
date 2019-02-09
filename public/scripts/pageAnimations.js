bb822608b116592196565cba7d0f7c20bfb98632
stephanLeece <stephan.leece@outlook.com>
2019-02-09 12:40:46 +0100

    add initial transform positions to screens, update slidein function

--- public/scripts/pageAnimations.js
+++ public/scripts/pageAnimations.js
@@ -1,10 +1,8 @@
-var topPage = $( ".content__screen--video" );
-$( ".nav__element--video" ).on( "click", function( event ) {
+var topPage = $( ".content__screen--artist" );
+$( ".nav__element--artist" ).on( "click", function( event ) {
   slideIn(topPage);
 });
 
-
-
 function slideIn(sectionClassName) {
-     sectionClassName.animate({left: '250px'});
+  sectionClassName.css({transform: 'translateY(0%)'});
 }
